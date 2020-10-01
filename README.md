8858afe526cae2d48b313d6221defe5d89671a97
Artemiy <38810090+OptimumDev@users.noreply.github.com>
2020-10-01 18:32:44 +0500

    Init commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
+![Heroku Deploy](https://github.com/romutchio/docs.github.io/workflows/Heroku%20Deploy/badge.svg)
+
 # docs.github.io
 Application for your personal documents (Scan and Original)
+
+
+# API
+Available at: [link](https://docs-io.herokuapp.com/greeting)
---
+++ frontend/src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import * as serviceWorker from './serviceWorker';
+
+import corgi from './images/BigCorgi.gif';
+
+import './index.css';
+
+ReactDOM.render(
+    <React.StrictMode>
+        <div className="App">
+            <header>
+                <h1>Hello there!</h1>
+                <h3>Work in progress...</h3>
+                <img src={corgi} alt='Doggo'/>
+            </header>
+        </div>
+    </React.StrictMode>,
+    document.getElementById('root')
+);
+
+serviceWorker.unregister();
