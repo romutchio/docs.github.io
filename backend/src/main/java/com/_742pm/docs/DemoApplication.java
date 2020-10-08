ce07bcca6d2ff8c8419e03caf716ea333ba02d89
Artemiy <38810090+OptimumDev@users.noreply.github.com>
2020-10-08 22:30:35 +0500

    Revert "add indus code"
    This reverts commit 4d5779529c60e7061a06eb5d465adcf6ef987592.

--- backend/src/main/java/com/_742pm/docs/DemoApplication.java
+++ backend/src/main/java/com/_742pm/docs/DemoApplication.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackendApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(BackendApplication.class, args);
+public class DemoApplication
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(DemoApplication.class, args);
     }
 
 }
