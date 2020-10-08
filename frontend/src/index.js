a3ab1c66815b673b25b4c4fa6fde63a5a314c4fd
Artemiy <38810090+OptimumDev@users.noreply.github.com>
2020-10-08 22:04:51 +0500

    refactor frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import corgi from './images/BigCorgi.gif';
+
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <div className="App">
+            <header>
+                <h1>Hello there!</h1>
+                <h3>Work in progress...</h3>
+                <img src={corgi} alt='Doggo'/>
+            </header>
+        </div>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
