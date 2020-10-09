bd04334ec60230ed4ddec465aa34453a72675b44
romutchio <budljanskiy@mail.ru>
2020-10-09 22:06:58 +0500

    fix annotation;

--- backend/src/main/java/com/_742pm/docs/controllers/UserController.java
+++ backend/src/main/java/com/_742pm/docs/controllers/UserController.java
@@ -3,14 +3,14 @@ package com._742pm.docs.controllers;
 import com._742pm.docs.models.User;
 import com._742pm.docs.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController
 public class UserController {
 
     @Autowired
