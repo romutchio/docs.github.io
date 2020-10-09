a56ebc45ed8bdcde14c006271f401a5a1d27e3a2
romutchio <budljanskiy@mail.ru>
2020-10-09 21:58:45 +0500

    add missed annotation;

--- backend/src/main/java/com/_742pm/docs/service/UserService.java
+++ backend/src/main/java/com/_742pm/docs/service/UserService.java
@@ -3,9 +3,11 @@ package com._742pm.docs.service;
 import com._742pm.docs.models.User;
 import com._742pm.docs.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class UserService implements IUserService {
 
     @Autowired
