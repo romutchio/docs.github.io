4ff8373249f661508b885909267a2bcdcfaf3f6b
romutchio <budljanskiy@mail.ru>
2020-10-17 21:49:05 +0500

    fixes;

--- backend/src/main/resources/schema-postgres.sql
+++ backend/src/main/resources/schema-postgres.sql
@@ -1,25 +1,25 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
--- DROP TABLE IF EXISTS users;
--- DROP TABLE IF EXISTS documents;
--- DROP TABLE IF EXISTS tags;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS documents;
+DROP TABLE IF EXISTS tags;
 
 CREATE TABLE users(
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     name VARCHAR(255),
     image_url VARCHAR(255)
 );
 
 CREATE TABLE documents(
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     name VARCHAR(255),
     user_id UUID
 );
 
 CREATE TABLE tags(
-    id serial PRIMARY KEY,
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     name VARCHAR(255),
     document_id UUID,
     user_id UUID
-)
+);
 
