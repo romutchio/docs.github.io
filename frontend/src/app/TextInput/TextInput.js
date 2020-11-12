510693c7560f3a5e8ce1b5f37acf644b6a0ace67
Artemiy <38810090+OptimumDev@users.noreply.github.com>
2020-11-12 21:42:09 +0500

    add create modal

--- frontend/src/app/TextInput/TextInput.js
+++ frontend/src/app/TextInput/TextInput.js
@@ -1,15 +1,32 @@
 import React from "react";
 import './TextInput.css';
 
-export default function TextInput({placeholder, className = '', hideLetters = false, inputRef = null, children, ...props}) {
-    return (
-        <input
-            type={hideLetters ? 'password' : 'text'}
-            className={`text-input ${className}`}
-            placeholder={placeholder}
-            ref={inputRef}
-            defaultValue={children}
-            {...props}
-        />
-    );
+export default class TextInput extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.ref = props.inputRef ?? React.createRef();
+    }
+
+    render() {
+        let {placeholder, className = '', inputRef, hideLetters = false, children, ...props} = this.props;
+        return (
+            <input
+                type={hideLetters ? 'password' : 'text'}
+                className={`text-input ${className}`}
+                placeholder={placeholder}
+                ref={this.ref}
+                defaultValue={children}
+                onKeyPress={this.onEnter}
+                {...props}
+            />
+        );
+    }
+
+    onEnter = e => {
+        if (e.key === 'Enter') {
+            this.ref.current.blur()
+        }
+    }
 }
