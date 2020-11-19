f5526f743ad5d32134d5b3a2ea9fdae5586c542d
A.Voi <voytsishevskiy@tochka.com>
2020-11-19 17:53:53 +0500

    add logs?

--- backend/src/main/java/com/_742pm/docs/Application.java
+++ backend/src/main/java/com/_742pm/docs/Application.java
@@ -2,10 +2,22 @@ package com._742pm.docs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
 @SpringBootApplication
 public class Application
 {
+    @Bean
+    public CommonsRequestLoggingFilter requestLoggingFilter() {
+        CommonsRequestLoggingFilter loggingFilter = new CommonsRequestLoggingFilter();
+        loggingFilter.setIncludeClientInfo(true);
+        loggingFilter.setIncludeQueryString(true);
+        loggingFilter.setIncludePayload(true);
+        loggingFilter.setMaxPayloadLength(64000);
+        return loggingFilter;
+    }
+
     public static void main(String[] args)
     {
         SpringApplication.run(Application.class, args);
