a71cbc43770b9916ae5bcc95c8d10fbdac886e06
Антон Войцишевский <grygry0@gmail.com>
2020-12-01 13:21:06 +0500

    remove bec;

--- backend/src/main/java/com/_742pm/docs/configs/SwaggerConfig.java
+++ backend/src/main/java/com/_742pm/docs/configs/SwaggerConfig.java
@@ -15,7 +15,7 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis( RequestHandlerSelectors.basePackage( "com._742pm.docs" ) )
                 .paths(PathSelectors.any())
                 .build();
     }
