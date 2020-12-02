7b04873adad8452d079428a175ca7418488b9ba3
Антон Войцишевский <grygry0@gmail.com>
2020-12-02 16:14:38 +0500

    add ine;

--- backend/src/main/resources/schema-postgres.sql
+++ backend/src/main/resources/schema-postgres.sql
@@ -1,13 +1,13 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-CREATE TABLE documents(
+CREATE TABLE IF NOT EXISTS documents(
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     name VARCHAR(255),
     user_id UUID,
     data text
 );
 
-CREATE TABLE tags(
+CREATE TABLE IF NOT EXISTS tags(
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     name VARCHAR(255),
     document_id UUID,
