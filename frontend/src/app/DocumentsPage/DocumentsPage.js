c5230071d431309198d7fcc74eecb141ba2f9ca5
Artemiy <38810090+OptimumDev@users.noreply.github.com>
2020-12-03 17:36:22 +0500

    YES!!!!!

--- frontend/src/app/DocumentsPage/DocumentsPage.js
+++ frontend/src/app/DocumentsPage/DocumentsPage.js
@@ -98,11 +98,12 @@ export default class DocumentsPage extends React.Component {
     }
 
     convertWordArrayToUint8Array = (wordArray) => {
-        var arrayOfWords = wordArray.hasOwnProperty("words") ? wordArray.words : [];
-        var length = wordArray.hasOwnProperty("sigBytes") ? wordArray.sigBytes : arrayOfWords.length * 4;
-        var uInt8Array = new Uint8Array(length), index=0, word, i;
-        for (i=0; i<length; i++) {
-            word = arrayOfWords[i];
+        const arrayOfWords = wordArray.hasOwnProperty("words") ? wordArray.words : [];
+        const length = wordArray.hasOwnProperty("sigBytes") ? wordArray.sigBytes : arrayOfWords.length * 4;
+        const uInt8Array = new Uint8Array(length);
+        let index=0;
+        for (let i = 0; i < length; i++) {
+            const word = arrayOfWords[i];
             uInt8Array[index++] = word >> 24;
             uInt8Array[index++] = (word >> 16) & 0xff;
             uInt8Array[index++] = (word >> 8) & 0xff;
